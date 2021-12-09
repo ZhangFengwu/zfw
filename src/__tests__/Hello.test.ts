2dc17fe9258dabf2249365d477419f5a54c96612
Fengwu Zhang <fengwu.zhang@binance.com>
2021-12-09 16:10:45 +0800

    v1.0.1

--- src/__tests__/Hello.test.ts
+++ src/__tests__/Hello.test.ts
@@ -1,5 +1,5 @@
-import  { Hello } from "../index"
+import { Hello } from '../index';
 
 test('My Hello', () => {
-    expect(Hello('Fred')).toBe('Hello Fred');
+  expect(Hello('Fred')).toBe('Hello Fred');
 });
